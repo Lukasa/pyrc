4c51bfbee0b8a06188ade1100a9c12fe7b73711f
Cory Benfield <lukasaoz@gmail.com>
2013-12-22 19:21:11 +0000

    And plumb the template through.

--- repeater/server.py
+++ repeater/server.py
@@ -7,16 +7,29 @@ The main HTTP server that powers pyrc. Right now, in this basic form, all it
 does is provide access to the websocket repeater.
 """
 from tornado.ioloop import IOLoop
-from tornado.web import Application
+from tornado.web import Application, RequestHandler
 
 from .repeater import Repeater
 import os.path
 
+
+class HomeHandler(RequestHandler):
+    """
+    Render the home page. For now there's no intelligent logic here, but there
+    might be at a later date.
+    """
+    def get(self):
+        self.render("index.html")
+
 # Define the top-level Tornado application.
 application = Application(
-    [(r"/wsock/", Repeater)],
+    [
+        (r"/wsock/", Repeater),
+        (r"/", HomeHandler),
+    ],
     gzip=True,
     static_path=os.path.join(os.path.dirname(__file__), "..", "static"),
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 if __name__ == '__main__':
