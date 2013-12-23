a95f67bd80ece98864c7189c913a5f2202b4bbf1
Cory Benfield <lukasaoz@gmail.com>
2013-12-23 09:53:14 +0000

    The IRC regex shouldn't be public.

--- static/js/irc.js
+++ static/js/irc.js
@@ -5,7 +5,7 @@ var irc = (function(window){
     var my = {};
 
     // The regex used to parse IRC messages. I swear, it works.
-    my.IRCRegex = /^(?:\:(\S+) )?(\S+)(?: ([^:]+))?(?: :(.*))?$/;
+    var IRCRegex = /^(?:\:(\S+) )?(\S+)(?: ([^:]+))?(?: :(.*))?$/;
 
     // parseIRCMsg takes an IRC message and returns an object with four fields:
     // the prefix, the command, the parameters, and the trailing section. These
@@ -13,7 +13,7 @@ var irc = (function(window){
     // "command" field is mandatory: the others may be set to undefined.
     my.parseIRCMsg = function(msg){
         msg = msg.trim();
-        var parsed = my.IRCRegex.exec(msg);
+        var parsed = IRCRegex.exec(msg);
         if (!parsed) return null;
         else return {
             prefix:   parsed[1],
